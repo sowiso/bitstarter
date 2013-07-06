4a524e3ed2520d15af03e8020a1950875d8127ca
Max Cohen <m.cohen@sowiso.nl>
2013-07-06 12:58:32 +0200

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var fs = require('fs');
+
+var idx = fs.readFileSync('index.html');
+var buffer = new Buffer(idx);
+
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
